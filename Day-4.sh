8aadd08de1cd5f31abb40315126866876193e4e4
viho-kernel <vihari.reddy1802@gmail.com>
2026-04-28 19:10:16 +0530

    latest commit

--- Day-4.sh
+++ Day-4.sh
@@ -23,7 +23,7 @@ log_message() {
     local level="$1"
     local message="$2"
     local timestamp
-    timestamp=$(date + "+%Y-%m-%d %H:%M:%S"))
+    timestamp=$((date + "+%Y-%m-%d %H:%M:%S"))
     echo "[$timestamp] $level $message " >> $LOG_FILE
 }
 
